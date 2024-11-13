6971d6a406d15691493eb290ea80b9d521bc8873
kaisambo <nelq.cz@gmail.com>
2024-11-13 15:00:15 +0300

    done

--- backend-gateway-client/src/main/java/com/itm/space/backend/client/BackendGatewayClientApplication.java
+++ backend-gateway-client/src/main/java/com/itm/space/backend/client/BackendGatewayClientApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendGatewayClientApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendGatewayClientApplication.class, args);
 	}
